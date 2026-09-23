69aaf1dc7c93d8917932189dbce065a091d85484
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-09-23 19:12:31 +0300

    initial source cxx tab utility

---
+++ source/cxx/tab/main.cxx
@@ -0,0 +1,12 @@
+/*
+    tab (or `spacetab`): command-line tool
+    \ to convert spaces into tabs but only
+    \ for lines that starting with spaces.
+*/
+// includes
+// -- std
+#include <iostream>
+
+int main( int argc, char *argv[] ) {
+    std::cout << argv[argc - argc] << std::endl;
+}
